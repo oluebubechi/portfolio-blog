36b821603e1f6615ffaaeb25f14efb5667f78e28
oluebubechi <emmanuelokechukwu3027@gmail.com>
2022-12-13 19:14:00 +0100

    ..

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/*============================ typing animation ======================== */
+var typed = new Typed(".typing", {
+    strings:["","Web Developer", "Graphic Designer", "Freelancer"],
+    typeSpeed:150,
+    BackSpeed:60,
+    loop:true
+})
